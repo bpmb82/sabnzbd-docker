6c12dacb4412f5cb16b60b69cf29034e3a8e14c9
bpmb82 <56978424+bpmb82@users.noreply.github.com>
2020-12-12 19:31:06 +0100

    Create healthcheck.sh

---
+++ healthcheck.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FILE=/config/config.xml
+
+until test -f $FILE; do sleep 1; done
+
+API=$(xml_grep --text_only 'ApiKey' $FILE)
+curl -f "http://localhost:8080/api/system/status?apikey=$API" || exit 1
