1c08d8ad9aa81694ee6a17c497d2168c75d95d04
bpmb82 <56978424+bpmb82@users.noreply.github.com>
2020-12-13 13:39:29 +0100

    Update healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-FILE=/config/config.xml
+FILE=/config/config.ini
 
 until test -f $FILE; do sleep 1; done
 
-API=$(xml_grep --text_only 'ApiKey' $FILE)
+API=$(cat /config/config.ini | grep 'api_key =' | egrep -v 'disable|rating' | awk -F '= ' '{print $2}')
 curl -f "http://localhost:8080/api/system/status?apikey=$API" || exit 1
